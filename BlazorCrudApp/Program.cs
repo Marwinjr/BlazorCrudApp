05f0d99f95d42a315f79da98e3aa9f82bf278d41
marwi <marwi@MARWINJR>
2025-03-26 14:39:58 -0600

    Add project files.

---
+++ BlazorCrudApp/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using BlazorCrudApp.Components;
+using BlazorCrudApp.Repository;
+using BlazorCrudApp.Data;
+
+namespace BlazorCrudApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+            builder.Services.AddDbContextFactory<EmployeeDBContext>(options =>
+            options.UseSqlServer(
+            builder.Configuration["ConnectionStrings:EmployeeContext"]
+            ));
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error", createScopeForErrors: true);
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
+
+
+
---
+++ BlazorCrudApp/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace BlazorCrudApp.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetById(int id);
+
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+
+        void Add(T entity);
+        void Delete(T Delete);
+        void Update(T entity);
+        T GetByID(int id);
+    }
+}
